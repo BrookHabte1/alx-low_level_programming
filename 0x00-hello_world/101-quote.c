73908d650870ed65c430444894c80f7e51149ac1
BrookHabte1 <brookhabte5@gamil.com>
2022-04-08 01:06:38 +0300

    better

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,9 @@
 #include <unistd.h>
+/**
+ * main - Displaying sizeof data types.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
 char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
