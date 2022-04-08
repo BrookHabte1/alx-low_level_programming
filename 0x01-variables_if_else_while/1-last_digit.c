da4749a1b23d9efe5c1aed9fcd6e76af183b5aee
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 01:09:18 +0300

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is %d ", n, n % 10);
 if (n % 10 > 5)
 printf("and is greater than 5");
-if(n == 0)
+else if(n == 0)
 printf("and is 0");
-if(n < 6&n != 0)
+else if(n < 6 && n != 0)
 printf("and is less than 6 and not 0");
 return (0);
 }
