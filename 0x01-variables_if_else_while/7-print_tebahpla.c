1a8276f5388c16546283a74ae17307efbe9cf43b
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 02:03:42 +0300

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - positive or negative.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+char y;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
