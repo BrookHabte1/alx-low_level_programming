20119ca7f388db00484287a50b63021ae17541d7
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 01:54:49 +0300

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - positive or negative.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
