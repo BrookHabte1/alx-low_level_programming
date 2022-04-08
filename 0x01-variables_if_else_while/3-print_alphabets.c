bdd900c2da51aae640e0f0ebc09f4a2ebfe8cbe6
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 01:41:07 +0300

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - positive or negative.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+char y;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar( x);
+}
+for (y = 'A'; y <= 'Z'; y++)
+{
+putchar( y);
+}
+putchar('\n');
+return (0);
+}
