30d15b930715f97d6ff880ab999a341ea13bb79a
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 00:47:39 +0300

    finished

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n >= 0)
 {
-if (n > 0){
-printf("%d is postive\n" , n);
+if (n > 0)
+printf("%d is postive\n", n);
+else
+printf("%d is zero\n", n);
 }
 else
-{
-printf("%d is zero\n" , n);
-}
-}
-else
-{
-printf("%d is negative\n" , n);
-}
+printf("%d is negative\n", n);
 return (0);
 }
