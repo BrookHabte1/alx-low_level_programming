3f8b831070ed34b407a63b6afeb76852bf8d799a
BrookHabte1 <brookhabte5@gamil.com>
2022-04-09 03:19:02 +0300

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+int x;
+int y;
+for (x = 0 ; x < 9 ; x++)
+{
+for (y = 1 ; y < 10 ; y++)
+{
+putchar(x + 48);
+putchar(y + 48);
+if (x == 8 && y == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
