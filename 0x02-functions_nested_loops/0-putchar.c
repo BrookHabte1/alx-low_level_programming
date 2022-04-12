b37e90e548141d4ef0a501f590396fe99de674f9
BrookHabte1 <brookhabte5@gamil.com>
2022-04-12 20:11:13 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - positive or negative.
+(*
+ * Return: 0 on success
+ */
+int main()
+{
+printf("_putchar\n");
+
+return 0;
+}
