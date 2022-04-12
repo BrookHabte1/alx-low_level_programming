951f8e1d5e111a885e5ede674b75f05590d4edfc
BrookHabte1 <brookhabte5@gamil.com>
2022-04-13 01:17:47 +0300

    Prints lowercase letters 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 void print_alphabet_x10(void)
 {
 char a;
-int b;
-while (a <= 10)
+int b = 1;
+while (b <= 10)
 {
 for (a = 'a'; a <= 'z'; a++)
 {
 _putchar(a);
 }
 _putchar('\n');
-a++;
+b++;
 }
 }
