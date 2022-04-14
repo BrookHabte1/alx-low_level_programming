05850325b0d70785c16785d490f1af065c4a875c
BrookHabte1 <brookhabte5@gamil.com>
2022-04-14 13:24:42 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers.
+ *
+ *@a: character.
+ * Return: 0 on success.
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
