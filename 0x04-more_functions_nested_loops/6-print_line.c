e23d4179f12623434342015c5e101d29ea7bf382
BrookHabte1 <brookhabte5@gamil.com>
2022-04-14 14:17:21 +0300

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers from 0 to 14 times 10.
+ *@n: character
+ *
+ * Return: Nothing.
+ */
+void print_line(int n)
+{
+int a;
+if (n > 0)
+{
+for (a = 1; a <= n ; a++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
