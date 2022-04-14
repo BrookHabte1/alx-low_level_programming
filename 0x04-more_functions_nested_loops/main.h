a29736edc270d94a3c2cd81055d2b02634c985ba
BrookHabte1 <brookhabte5@gamil.com>
2022-04-14 12:51:03 +0300

    Fixed the issue of main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 /*
-* File: main.h
-* Desc: Header file containing declarations for all functions.
-*/
+ * File: main.h
+ * Desc: Header file containing declarations for all functions.
+ */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,7 +12,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_diagonal(int n);
 void print_square(int size);
+void print_diagonal(int n);
 void print_triangle(int size);
+void print_square(int size);
+void print_number(int n);
 #endif
