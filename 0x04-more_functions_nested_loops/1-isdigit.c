b4c61024f54f2bec5b2bad7113a4ba21f28e9bf6
BrookHabte1 <brookhabte5@gamil.com>
2022-04-14 13:03:19 +0300

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,13 @@
 /**
  * _isdigit - check if digit.
  *
- *@c: character
+ *@c: digit
  *
  * Return: 0 on success.
  */
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
+if (c >= '0' && c <= '9')
 return (1);
 
 return (0);
