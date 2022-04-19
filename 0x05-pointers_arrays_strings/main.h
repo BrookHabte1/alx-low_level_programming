fa2bf51f45c5587a4e1aff5b352b7db8c5af5b02
BrookHabte1 <brookhabte5@gamil.com>
2022-04-19 23:10:32 +0300

    maintained main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
  * File: main.h
  * Desc: Header file containing declarations for all functions.
  */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
