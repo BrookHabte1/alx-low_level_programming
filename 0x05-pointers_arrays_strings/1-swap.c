9cfff3e90e5e08a19efb71708c557ddc3d69610b
BrookHabte1 <brookhabte5@gamil.com>
2022-04-19 13:49:23 +0300

    Added a file to swap to integers.

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,11 +2,13 @@
 /**
  *swap_int - swaps the valur of teo integers.
  *@x: digit.
+ *@a: pointer.
+ *@b: pointer.
  *Return: it eill not return anything./\
  */
 void swap_int(int *a, int *b)
 {
-    int x = *a;
-    *a = *b;
-    *b = x;
+int x = *a;
+*a = *b;
+*b = x;
 }
