d4c6406911912625af001cc983299138a89d7ea6
BrookHabte1 <brookhabte5@gamil.com>
2022-04-19 14:07:10 +0300

    Added a file to return string length.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  *_strlen - returns the length of a function.
  *@s: is a character.
@@ -7,5 +8,5 @@
  */
 int _strlen(char *s)
 {
-strlen(*str);
+strlen(*s);
 }
