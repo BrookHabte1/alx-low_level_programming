998c77925bc6896ff173d24fbb086895df83dd20
BrookHabte1 <brookhabte5@gamil.com>
2022-04-20 09:50:42 +0300

    finilzed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,24 @@
 #include "main.h"
-
 /**
- * print_diagonal - printing in diagonal
- * @n: input
+ * print_diagonal - prints the numbers from 0 to 14 times 10.
+ *@n: character
  *
- * Return: void
+ * Return: Nothing.
  */
-
-
 void print_diagonal(int n)
 {
-int i, j;
-if (n <= 0)
-_putchar('\n');
-for (i = 0 ; i < n ; i++)
+int a,b;
+if (n > 0)
 {
-for (j = 0 ; j < i ; j++)
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
 {
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
 }
-
+}
+_putchar('\n');
 }
